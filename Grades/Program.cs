5949832829084f248da79489898cbc60097329ae
michalpom <michalpom@gmail.com>
2018-06-27 20:14:58 +0200

    Branching (if statments)

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -19,12 +19,12 @@ namespace Grades
 
             //book.NameChanged += new NameChangedDelegate(OnNameChanged);
             //book.NameChanged += new NameChangedDelegate(OnNameChanged2);
-            book.NameChanged += OnNameChanged;
+            //book.NameChanged += OnNameChanged;
             
 
 
-            book.Name = "Scott's Grade Book";
-            book.Name = null; //zabaepieczone przed tym w property w GradeBook
+            //book.Name = "Scott's Grade Book";
+            //book.Name = null; //zabaepieczone przed tym w property w GradeBook
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
@@ -36,8 +36,9 @@ namespace Grades
             GradeStatististics stats = book.ComputeStatistics();
             Console.WriteLine(book.Name);
             WriteResult("Average",stats.AverageGrade);
-            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Highest", stats.HighestGrade);
             WriteResult("Lowest",stats.LowestGrade);
+            WriteResult("Grade", stats.LetterGrade);
 
             //GradeBook book2 = book; //= new GradeBook();
             //book2.AddGrade(75);
@@ -53,23 +54,28 @@ namespace Grades
         //    Console.WriteLine($"Grade book changing name from {existingName} to {newName}");
 
         //}
-        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        //static void OnNameChanged(object sender, NameChangedEventArgs args)
+        //{
+        //    Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+
+        //}
+
+
+
+        //static void WriteResult(string description, int  result)
+        //{ //overload methods
+        //    Console.WriteLine(description + ": " + result);
+        //}
+        static void WriteResult(string description, string result)
         {
-            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
-
+            
+            Console.WriteLine($"{description}: {result}", description, result);
         }
-
-
-
-        static void WriteResult(string description, int  result)
-        { //overload methods
-            Console.WriteLine(description + ": " + result);
-        }
-
         static void WriteResult(string description, float result)
         {
             //Console.WriteLine(description + ": " + result);
-            Console.WriteLine("{0}: {1}", description, result);
+            //Console.WriteLine("{0}: {1}", description, result);
+            Console.WriteLine($"{description}: {result:F2}", description, result);
         }
     }
 
